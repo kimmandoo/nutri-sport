7507bde81c92c94eb90ce54bde597582fe9c1022
kimmandoo <mingyu5675@gmail.com>
2025-05-29 23:30:30 +0900

    feat: splash screen, launch screen

--- composeApp/src/androidMain/kotlin/com/kimmandoo/nutrisport/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kimmandoo/nutrisport/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        // splash screen은 setContent전에 있어야됨
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         setContent {
             App()
         }
